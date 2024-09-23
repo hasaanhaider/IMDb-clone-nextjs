2718ddee911c26ac81b9b266fccaf8ea6367192d
hasaanhaider <bejantal456@gmail.com>
2024-09-23 23:19:57 +0500

    about us page developed

---
+++ src/app/about/page.jsx
@@ -0,0 +1,53 @@
+const page = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed repellat,
+        dolores adipisci maiores quod unde modi reiciendis numquam vero
+        perspiciatis, facere harum inventore error cumque veritatis deleniti
+        magnam eius sapiente mollitia. Nesciunt iure vero laborum vitae,
+        perspiciatis magnam libero, alias qui minima cum earum. Labore fuga
+        molestiae reiciendis eos sequi ratione consectetur soluta iste
+      </p>
+      <p>
+        necessitatibus ipsam explicabo eligendi, consequatur et ipsum.
+        Reiciendis, quos sapiente. Distinctio quo expedita quas perferendis,
+        similique, nisi repudiandae voluptatem perspiciatis beatae placeat
+        accusantium quis quibusdam magni facilis itaque! Rem voluptas adipisci
+        commodi reiciendis quos sapiente a velit, id autem. Cum officiis dolor
+        ipsa quos tempora modi perspiciatis reprehenderit, delectus assumenda
+        error odio earum quo quasi dicta placeat explicabo enim inventore
+      </p>
+      <p>
+        reiciendis repudiandae doloremque, voluptatum itaque! Aut odio sunt,
+        officiis ipsam iste quaerat? Nesciunt obcaecati alias cupiditate! Illum
+        harum praesentium saepe, enim odit nesciunt vitae debitis sunt corporis
+        unde eveniet velit voluptate impedit, ea officia laboriosam a doloremque
+        minus aspernatur nulla consequatur. Nihil nobis soluta eos obcaecati
+        maiores voluptatem fugit. Possimus beatae molestias architecto harum,
+        est reiciendis earum, illum corrupti voluptas quos veniam
+      </p>
+      <p>
+        necessitatibus, doloribus ducimus obcaecati blanditiis sunt dicta
+        asperiores. Deleniti odio voluptates odit officiis! Perspiciatis ipsa,
+        sunt aspernatur ducimus ea temporibus ratione vero error dolores eius
+        possimus officiis debitis, et exercitationem. Numquam laborum at
+        necessitatibus est debitis quo pariatur soluta nostrum! Modi repellendus
+        animi ratione amet fugit dignissimos quas corrupti, quia impedit
+        recusandae natus maxime deserunt molestiae temporibus tempora magnam
+        quasi quis cumque quidem. Ad ullam, reiciendis molestias est amet rerum
+        fugiat pariatur quidem quibusdam aliquid labore quaerat quos et{" "}
+      </p>
+      <p>
+        cum? Cum commodi et optio dolore rerum possimus. Molestiae dolorem
+        officiis unde ex, iusto mollitia quae quam a officia perspiciatis eos
+        minus omnis inventore praesentium nostrum doloribus adipisci, error
+        fugit! Officiis repellat, totam nam ipsa unde cupiditate ducimus rerum
+        veniam cum illo autem quod laudantium.
+      </p>
+    </div>
+  );
+};
+
+export default page;
