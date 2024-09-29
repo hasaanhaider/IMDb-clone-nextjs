1ca7bd9207743cf54656d119d2f632f6874080d6
hasaanhaider <bejantal456@gmail.com>
2024-09-29 15:06:40 +0500

    make the navbar of trending and top rated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-import Providers from "./Providers";
+import Navbar from "./components/Navbar";
+import Providers from "./Providers";  
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
           {/* header */}
           <Header />
           {/* navbar */}
+          <Navbar />
 
           {/* search bar */}
           {children}
