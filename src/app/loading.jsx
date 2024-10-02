0765178d4ce5f38c1dc1fbd07de90bad02ae272e
hasaanhaider <bejantal456@gmail.com>
2024-10-02 22:52:16 +0500

    loading effect added

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="flex justify-center">
+      <img className="h-96" src="spinner2.svg" alt="spinner" />
+    </div>
+  );
+};
+
+export default loading;
